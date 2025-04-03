e9d1104e46cdeb9b3ff25299323901e53971f210
GabrielMaurici0 <mauriciogabriel332@gmail.com>
2025-04-03 09:23:33 -0300

    Create: Criados os casos de login correto e incorreto

---
+++ README.md
@@ -0,0 +1,43 @@
+# Como iniciar um projeto utilizando Playwright
+
+1. Instalar o Yarn utilizando o comando:
+    ```
+    npm install --global yarn
+    ```
+
+2. Verificar a instalação do Yarn com:
+    ```
+    yarn --version
+    ```
+
+3. Inicializar um novo projeto com Yarn:
+    ```
+    yarn init
+    ```
+
+4. Instalar a infraestrutura do Playwright
+    ```
+    npx playwright install
+    ```
+
+5. Criar o arquivo playwright.config.ts na root da página
+
+
+# Como executar um teste
+
+1. Criar a pasta 
+    ```
+    tests
+    ```
+
+2. Dentro da pasta /tests/ deve ter ao menos um arquivo XXXX.spec.ts 
+
+3. Executar o comando abaixo para executar os testes normalmente
+    ```
+    npx playwright test
+    ```
+
+    Caso seja necessário Debugar utilizar o comando:
+    ```
+    npx playwright test --debug
+    ```
---
+++ tests/login.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from '@playwright/test';
+import { log } from 'console';
+
+//Test -> Se trata do todo, no caso, o bloco que contém o método de teste
+
+//Expect -> Se trata do que se espera do teste, o resultado esperado
+
+//await -> Se trata de uma palavra reservada do JavaScript que permite que o código aguarde a execução de uma função assíncrona 
+// antes de continuar a execução do código. Isso é útil quando você precisa esperar por uma operação assíncrona, como uma chamada 
+// de rede ou uma operação de leitura de arquivo, antes de prosseguir com o restante do código.
+
+//test('descrição'), async ({ page -> acesso ao contexto do navegador })=>{}
+
+
+test('Login Correto', async ({ page }) => {
+    
+    // Vai para a pagina de login / Acessa a url
+    await page.goto('https://www.moozcobranca.com.br/homologacao/servlet/hsiscobra');
+
+
+    //Preenche os dados com base no arquivo dotenv
+    await page.fill('//*[@id="_USUCODC"]', process.env.USUARIO!);
+    await page.fill('#_USUSEN', process.env.SENHA!);
+
+    //Clica no botão de Login
+    await page.locator('//*[@id="TABLE"]/tbody/tr[6]/td[3]/input[1]').click();
+        
+
+    const confirmar = page.locator('//*[@id="TABLE_DERRUBA_SESSAO"]/tbody/tr[5]/td[3]/input[1]');
+
+    if (confirmar) {
+        await confirmar.click();
+    }
+
+    await expect(page).toHaveURL('https://www.moozcobranca.com.br/homologacao/servlet/hbranco');
+
+});
+
+test('Login Incorreto', async ({ page }) => {
+    await page.goto('https://www.moozcobranca.com.br/homologacao/servlet/hsiscobra');
+
+    await page.fill('//*[@id="_USUCODC"]', process.env.USUARIO!);
+    await page.fill('#_USUSEN', 'abcdefg');
+
+    await page.locator('//*[@id="TABLE"]/tbody/tr[6]/td[3]/input[1]').click();
+
+    await expect(page.locator('//*[@id="swal2-html-container"]')).toHaveText('Os dados informados estão incorretos.');
+
+});
